8d879731e4a4f6066097fd0f3903265fab6be805
Emir-de-la-moquette <shanky.yt@gmail.com>
2025-10-15 14:40:11 +0200

    module topbar complete (prete à être travaillée sur le connexion)

--- bourge2028/src/app/components/Topbar.jsx
+++ bourge2028/src/app/components/Topbar.jsx
@@ -5,6 +5,10 @@ export default function Topbar({ title = "Bourges 2028"}){
     return (
         <header className="topbar">
             <h1>{title}</h1>
+            <a className="connect" href="">
+                <img src="/images/tete.png" alt="tete" />
+                <p>Se connecter</p>
+            </a>
         </header>
     );
 };
