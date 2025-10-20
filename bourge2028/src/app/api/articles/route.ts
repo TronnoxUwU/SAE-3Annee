e968f0467e32b639ada864dbf88f24acf4b2f397
RICHARD Baptiste <143808981+TronnoxUwU@users.noreply.github.com>
2025-10-20 12:04:41 +0200

    deplacement et route fonctionnel

---
+++ bourge2028/src/app/api/articles/route.ts
@@ -0,0 +1,72 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { deserializeArticle } from "@/lib/deserializers";
+import { serializeArticle } from "@/lib/serializers";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const data = deserializeArticle(body);
+
+    const article = await prisma.article.create({
+      data,
+      include: {
+        composants: {
+          include: {
+            titre: true,
+            paragraphe: true,
+            image: true,
+            caroussels: { include: { images: true } },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(serializeArticle(article), { status: 201 });
+  } catch (error: any) {
+    console.error("Erreur création article:", error);
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
+
+export async function GET() {
+  try {
+    // Récupérer tous les articles avec composants et documents liés
+    const articles = await prisma.article.findMany({
+      include: {
+        composants: {
+          include: {
+            paragraphe: true,
+            titre: true,
+            image: true,
+            caroussels: {
+              include: {
+                images: true,
+              },
+            },
+          },
+        },
+        contenir: {
+          include: {
+            document: true,
+          },
+        },
+      },
+      orderBy: {
+        id: "asc",
+      },
+    });
+
+    // Sérialisation propre pour l'API
+    const serialized = articles.map(serializeArticle);
+
+    return NextResponse.json(serialized, { status: 200 });
+  } catch (error) {
+    console.error("Erreur GET /api/articles :", error);
+    return NextResponse.json(
+      { error: "Impossible de récupérer les articles" },
+      { status: 500 }
+    );
+  }
+}
