443817b43429119a3693aa40162377907fa51ca0
TristanChaloine <tristan.chaloine@gmail.com>
2025-11-07 11:15:28 +0100

    modif de/serializer cat en adequation avec BD

--- bourge2028/src/lib/serializers/categorieSerializer.ts
+++ bourge2028/src/lib/serializers/categorieSerializer.ts
@@ -1,21 +1,15 @@
-import type { Categorie, Tag } from "@prisma/client";
+import type { Categorie} from "@prisma/client";
 
 
 export const serializeCategorie = (
   categorie: Categorie & {
     children?: (Categorie & {
       children?: Categorie[];
-      tags?: Tag[];
-    })[];
-    tags?: Tag[];
+    })[] | undefined;
   }
 ) => ({
   id: categorie.id,
   nom: categorie.nom,
   parentId: categorie.parentId ?? null,
-  tags: categorie.tags?.map(tag => ({
-    id: tag.id,
-    nom: tag.nom,
-  })) || [],
   children: categorie.children?.map(serializeCategorie) || [],
 });
